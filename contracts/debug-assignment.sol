6fd246a3659e3b3428b649a2608c5c53cfbe7e13
Bayz.ng <ayofe70@gmail.com>
2024-05-02 23:01:33 -0700

    first commit

---
+++ contracts/debug-assignment.sol
@@ -0,0 +1,13 @@
+pragma soidity >0.7.0 <0.9.0;
+
+contractt Storage {
+
+    unit number
+
+    function sore(uint256 num) puublic {
+        number = num
+    }
+
+    function retrieve( public view returns (uint256){
+        return number
+    }
